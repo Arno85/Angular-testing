ee1157e703ab4f89852d6dbed591b3056725b2f4
Arnaud <arnaud.martin85@gmail.com>
2020-02-22 20:22:08 -0500

    e2e

---
+++ angular-testing-course-1-start/cypress/integration/home.test.js
@@ -0,0 +1,36 @@
+import { createYield } from "typescript";
+
+describe('Home Page', () => {
+  beforeEach(() => {
+    // Link Stubs data
+    cy.fixture('courses.json').as('coursesJSON');
+    // Mock a server
+    cy.server();
+    // Create event linked to a route
+    cy.route('/api/courses', '@coursesJSON').as('courses');
+
+    cy.visit('/');
+  });
+
+  it('should display a list of courses', () => {
+    cy.contains('All Courses');
+
+    // Wait http call
+    cy.wait('@courses');
+    cy.get('mat-card').should('have.length', 9);
+  });
+
+  it('should display the adanced courses', () => {
+    cy.get('.mat-tab-label').should('have.length', 2);
+    cy.get('.mat-tab-label')
+      .last()
+      .click();
+    cy.get('.mat-tab-body-active .mat-card-title')
+      .its('length')
+      .should('be.gt', 1);
+    cy.get('.mat-tab-body-active .mat-card-title')
+      .first()
+      .should('contain', 'Angular Security Course');
+  });
+
+});
